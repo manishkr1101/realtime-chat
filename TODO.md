5113058b7fc46573b10864296f04508025be1965
Manish Kumar <manishkumarsh120@gmail.com>
2021-04-17 15:35:39 +0530

    connection established in local network

--- TODO.md
+++ TODO.md
@@ -1,2 +1,4 @@
 # TODO
 - [x] Build basic input/output system.
+- [ ] Establish connection between two nodes.
+- [ ] Send and receive text.
--- chat.js
+++ chat.js
@@ -1,4 +1,29 @@
-const {input} = require('./util')
+const { input } = require('./util')
+const net = require('net')
+
+// accepts incoming socket connection asynchronously
+function accept(server) {
+    return new Promise((resolve, reject) => {
+        server.on('connection', socket => {
+            resolve(socket)
+        })
+    })
+}
+
+// connects to a socket server asynchronously
+function connect(port) {
+    const socket = new net.Socket();
+    socket.connect(port);
+    
+    return new Promise((resolve, reject) => {
+        socket.on('connect', function() {
+            resolve(socket)
+        })
+        socket.on('error', function(err) {
+            reject({err: err})
+        })
+    })
+}
 
 async function app() {
     // ask for initialisation
@@ -6,13 +31,40 @@ async function app() {
     console.log("1. Start conversation.");
     console.log("2. Join conversation.")
     const ans = await input("Enter choice : ")
-    if(ans == "1") {
+    let socket = new net.Socket();
+    if (ans == "1") {
         // start server and listen for other party to join
-        console.log("Waiting for someone to connect...");
+        
+        var server = net.createServer();
+        server.listen(5000);
+
+        server.on('listening', function () {
+            
+            console.log("Waiting for someone to connect...");
+        });
+        server.on('error', function (err) {
+            console.log("error", err);
+        });
+        server.on('close', function() {
+            console.log('server closed')
+        })
+
+
+        socket = await accept(server);
+
+        console.log('somone got connected')
     }
-    else if(ans == "2") {
+    else if (ans == "2") {
         // join conversation with given information
         console.log("Enter details to connect");
+        const port = await input("Port : ");
+        socket = await connect(port);
+
+        console.log('connection established')
+
+    }
+    else {
+        process.exit(1);
     }
 
     /**
@@ -20,42 +72,45 @@ async function app() {
      * @param {string} msg Message to print
      * @param {boolean} flag false if printing own message otherwise true
      */
-    function renderMessage(msg, flag=true) {
-        if(flag == false) {
-            process.stdout.moveCursor(0,-1);
+    function renderMessage(msg, flag = true) {
+        if (flag == false) {
+            process.stdout.moveCursor(0, -1);
         }
         process.stdout.cursorTo(0);
-        const dy = Math.ceil(msg.length/process.stdout.columns);
-        process.stdout.moveCursor(0,-1*dy);
+        const dy = Math.ceil(msg.length / process.stdout.columns);
+        process.stdout.moveCursor(0, -1 * dy);
         process.stdout.write(msg);
         process.stdout.clearScreenDown();
-        process.stdout.moveCursor(0,1);
+        process.stdout.moveCursor(0, 1);
         process.stdout.cursorTo(0);
         // process.stdout.clearLine(1);
         process.stdout.write("\n");
         // process.stdout.moveCursor(0,1);
-        if(flag) {
+        if (flag) {
             process.stdout.write("> ");
         }
     }
-    // simulating incoming message
-    setTimeout(() => {
-        renderMessage("new message 1")
-        renderMessage("message 2");
-    }, 1000);
 
-    setTimeout(() => {
-        renderMessage("bob : new message 3");
-    }, 10000);
-    
-    process.stdout.write('\n');
-    while(true) {
-        const res = await input("> ");
-        if(res == 'exit') {
-            process.exit(0);
-        }
-        renderMessage(`me  : ${res}`,false);
+
+    function closeApp() {
+        if(server) server.close();
+        socket.destroy()
+        process.exit(0);
     }
+
+    socket.on('close', function() {
+        renderMessage('connection closed')
+    })
+
+    process.stdout.write('\n');
+    while (true) {
+        const res = await input("> ");
+        if (res == 'exit') {
+            closeApp();
+        }
+        renderMessage(`me  : ${res}`, false);
+    }
+
 }
 
 app();
